d816efdf74b87e6f77ed3741f75a63e261f84e99
Administrator <soongon@a.a>
2019-01-22 12:12:07 +0900

    initial commit

---
+++ src/main/java/kr/re/kitri/hello/FunctionalDemoFirst3.java
@@ -0,0 +1,13 @@
+package kr.re.kitri.hello;
+
+public class FunctionalDemoFirst3 {
+
+    public static void main(String[] args) {
+
+        Runnable runnable = () -> { System.out.println("thread run.."); };
+
+        Thread t = new Thread(runnable);
+        t.start();
+        System.out.println("main thread run..");
+    }
+}
---
+++ src/main/java/kr/re/kitri/hello/MyThread.java
@@ -0,0 +1,9 @@
+package kr.re.kitri.hello;
+
+public class MyThread implements Runnable {
+
+    public void run() {
+        System.out.println("my thead run..");
+    }
+
+}
